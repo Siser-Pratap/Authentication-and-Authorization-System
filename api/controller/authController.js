72cd2b451882deb6297ca5b92a43f401723a38a4
Siser Pratap <siserinsevoc@gmail.com>
2024-09-25 10:14:04 +0530

    created bcryptjs hashedpassword for users

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,11 +1,13 @@
 import user from "../models/user.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async(req, res)=>{
     console.log(req.body);
     
     const {username, password, email} = req.body;
+    const hashedPassword =bcryptjs.hashSync(password, 10);
 
-    const newUser = new user({username, password, email});
+    const newUser = new user({username, password:hashedPassword, email});
     try {
         await newUser.save();
         res.status(201).json({message:"User created Successfully"});
